9863226446dd7e8b7ddb24696f85548ddfeff8c2
Judah Tamirirashe Chisare <66766647+JudahCT@users.noreply.github.com>
2021-02-24 23:29:41 -0800

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,31 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from . import views
 
 urlpatterns = [
-    path('',views.home,name="home"),
-    path('login',views.login,name="login"),
-    path('portal',views.portal,name="portal"),
-    path('mainPortal',views.mainPortal,name="mainPortal"),
-    path('router',views.router,name="router"),
-    path('mifi',views.mifi,name="mifi"),
-    path('regUser',views.regUser,name="regUser"),
-    path('items',views.cartItems,name="items"),
-    path('sales',views.sales,name="sales"),
-    path('addRouter',views.addRouter,name="addRouter"),
-    path('addMifi',views.addMifi,name="addMifi"),
-    path('myCart',views.myCart,name="myCart"),
-    path('regItems',views.regItems,name="regItems"),
-
-    path('delete/<int:id>',views.delete,name="delete"),
-    
-    path('deleteItems/<int:id>',views.deleteItems,name="deleteItems"),
-    path('purchase/<int:id>',views.purchase,name="purchase"),
-    path('checkOut/<int:id>',views.checkOut,name="checkOut"),
-
-
-
-    
-
+    path('',include('store.urls')),
+    path('admin/', admin.site.urls),
 ]
